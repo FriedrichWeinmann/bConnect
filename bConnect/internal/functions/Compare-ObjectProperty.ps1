0a4638170e9156fab112e8119d4f932d4d9124f9
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-08-14 07:17:46 +0200

    Added comments, restyled
    No aliases
    Where-Object optimized
    Object return streamlined

--- bConnect/internal/functions/Compare-ObjectProperty.ps1
+++ bConnect/internal/functions/Compare-ObjectProperty.ps1
@@ -1,22 +1,43 @@
-Function Compare-ObjectProperty {
-    Param(
-        [PSObject]$ReferenceObject,
-        [PSObject]$DifferenceObject 
-    )
-    $objprops = $ReferenceObject | Get-Member -MemberType Property,NoteProperty - | % Name -WhatIf:$False
-    $objprops += $DifferenceObject | Get-Member -MemberType Property,NoteProperty | % Name -WhatIf:$False
-    $objprops = $objprops | Sort | Select -Unique
-    $diffs = @()
-    foreach ($objprop in $objprops) {
-        $diff = Compare-Object $ReferenceObject $DifferenceObject -Property $objprop
-        if ($diff) {            
-            $diffprops = @{
-                PropertyName=$objprop
-                RefValue=($diff | ? {$_.SideIndicator -eq '<='} | % $($objprop) -WhatIf:$False) 
-                DiffValue=($diff | ? {$_.SideIndicator -eq '=>'} | % $($objprop) -WhatIf:$False) 
-            }
-            $diffs += New-Object PSObject -Property $diffprops
-        }        
-    }
-    if ($diffs) {return ($diffs | Select PropertyName,RefValue,DiffValue)}     
+function Compare-ObjectProperty
+{
+<#
+	.SYNOPSIS
+		Generates a list of properties that do not have equal value between two objects.
+	
+	.DESCRIPTION
+		Generates a list of properties that do not have equal value between two objects.
+	
+	.PARAMETER ReferenceObject
+		The first object to compare the properties of.
+	
+	.PARAMETER DifferenceObject
+		The second object to compare the properties of.
+	
+	.EXAMPLE
+		PS C:\> Compare-ObjectProperty $item1 $item2
+	
+		Generates a list of all properties that do not have the same value between $item1^and $item2
+#>
+	[CmdletBinding()]
+	param (
+		$ReferenceObject,
+		
+		$DifferenceObject
+	)
+	$objprops = $ReferenceObject | Get-Member -MemberType Property, NoteProperty - | ForEach-Object Name -WhatIf:$False
+	$objprops += $DifferenceObject | Get-Member -MemberType Property, NoteProperty | ForEach-Object Name -WhatIf:$False
+	$objprops = $objprops | Sort-Object | Select-Object -Unique
+	$diffs = @()
+	foreach ($objprop in $objprops)
+	{
+		$diff = Compare-Object $ReferenceObject $DifferenceObject -Property $objprop
+		if ($diff)
+		{
+			[pscustomobject]@{
+				PropertyName = $objprop
+				RefValue	 = ($diff | Where-Object SideIndicator -eq '<=' | ForEach-Object $($objprop) -WhatIf:$False)
+				DiffValue    = ($diff | Where-Object SideIndicator -eq '=>' | ForEach-Object $($objprop) -WhatIf:$False)
+			}
+		}
+	}
 }
