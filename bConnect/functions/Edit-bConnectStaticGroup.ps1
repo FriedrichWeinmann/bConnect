bbdbfecf56ea380466eaa47fbbb6f9f02ab0f307
tobias.mueller@schriesheim.de <29426934+MuellerSchriesheim@users.noreply.github.com>
2019-03-19 10:25:41 +0100

    Bugfix who Prevents the Cmdlet from Adding Clients correctly

--- bConnect/functions/Edit-bConnectStaticGroup.ps1
+++ bConnect/functions/Edit-bConnectStaticGroup.ps1
@@ -1,77 +1,80 @@
 ﻿
 function Edit-bConnectStaticGroup
 {
-<#
+    <#
 	.SYNOPSIS
 		Updates a existing StaticGroup.
-	
+
 	.DESCRIPTION
 		Updates a existing StaticGroup.
-	
+
 	.PARAMETER StaticGroup
 		Valid modified StaticGroup
-	
+
 	.OUTPUTS
 		StaticGroup
 #>
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification="Test-PSFShouldProcess is used instead of ShouldProcess.")]
-	[CmdletBinding(ConfirmImpact = 'Medium', SupportsShouldProcess = $true)]
-	param (
-		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-		[PSCustomObject]
-		$StaticGroup
-	)
-	
-	begin
-	{
-		Assert-bConnectConnection
-	}
-	process
-	{
-		If (Test-Guid $StaticGroup.Id)
-		{
-			# bms2016r1
-			# We can not send the whole object because of not editable fields.
-			# So we need to create a new one with editable fields only...
-			# And as this might be too easy we face another problem: we are only allowed to send the changed fields :(
-			# Dirty workaround: reload the object and compare new vs. old
-			$oldGroup = Get-bConnectStaticGroup -StaticGroup $StaticGroup.Id
-			$oldGroup = ConvertTo-Hashtable $oldGroup
-			
-			$newGroup = @{ Id = $StaticGroup.Id }
-			$propertyList = @(
-				"ParentId",
-				"Name",
-				"EndpointIds",
-				"Comment"
-			)
-			$StaticGroup = ConvertTo-Hashtable $StaticGroup
-			
-			$endpointIds = @()
-			Foreach ($endpointID in $StaticGroup.EndpointIds)
-			{
-				$endpointIds += $endpointID.Id
-			}
-			$StaticGroup.EndpointIds = $endpointIds
-			
-			Foreach ($property in $propertyList)
-			{
-				If ($StaticGroup[$property] -ine $oldGroup[$property])
-				{
-					$newGroup += @{ $property = $StaticGroup[$property] }
-				}
-			}
-			
-			if (Test-PSFShouldProcess -PSCmdlet $PSCmdlet -Target $StaticGroup.Id -Action 'Edit Static Group')
-			{
-				Invoke-bConnectPatch -Controller "StaticGroups" -objectGuid $StaticGroup.Id -Data $newGroup | Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
-			}
-			else
-			{
-				Write-PSFMessage -Level Verbose -Message "Edit Static Group"
-				foreach ($k in $newGroup.Keys) { Write-PSFMessage -Level SomewhatVerbose -Message "$k : $($newGroup[$k])" }
-			}
-		}
-		
-	}
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSShouldProcess", "", Justification = "Test-PSFShouldProcess is used instead of ShouldProcess.")]
+    [CmdletBinding(ConfirmImpact = 'Medium', SupportsShouldProcess = $true)]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [PSCustomObject]
+        $StaticGroup
+    )
+
+    begin
+    {
+        Assert-bConnectConnection
+    }
+    process
+    {
+        If (Test-Guid $StaticGroup.Id)
+        {
+            # bms2016r1
+            # We can not send the whole object because of not editable fields.
+            # So we need to create a new one with editable fields only...
+            # And as this might be too easy we face another problem: we are only allowed to send the changed fields :(
+            # Dirty workaround: reload the object and compare new vs. old
+            $oldGroup = Get-bConnectStaticGroup -StaticGroup $StaticGroup.Id
+            $oldGroup = ConvertTo-Hashtable $oldGroup
+
+            $newGroup = @{ Id = $StaticGroup.Id }
+            $propertyList = @(
+                "ParentId",
+                "Name",
+                "EndpointIds",
+                "Comment"
+            )
+
+
+            $endpointIds = @()
+
+            Foreach ($endpointID in $StaticGroup.EndpointIds)
+            {
+                $endpointIds += $endpointID
+            }
+
+            $StaticGroup = ConvertTo-Hashtable $StaticGroup
+            $StaticGroup.EndpointIds = $endpointIds
+
+            Foreach ($property in $propertyList)
+            {
+                If ($StaticGroup[$property] -ine $oldGroup[$property])
+                {
+                    $newGroup += @{ $property = $StaticGroup[$property] }
+                }
+            }
+
+            if (Test-PSFShouldProcess -PSCmdlet $PSCmdlet -Target $newGroupGet.Id -Action 'Edit Static Group')
+            {
+                Invoke-bConnectPatch -Controller "StaticGroups" -objectGuid $newGroup.Id -Data $newGroup | Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
+            }
+            else
+            {
+                Write-PSFMessage -Level Verbose -Message "Edit Static Group"
+                foreach ($k in $newGroup.Keys) { Write-PSFMessage -Level SomewhatVerbose -Message "$k : $($newGroup[$k])" }
+            }
+        }
+
+    }
 }
--- bConnect/functions/Get-bConnectStaticGroup.ps1
+++ bConnect/functions/Get-bConnectStaticGroup.ps1
@@ -1,88 +1,88 @@
 ﻿function Get-bConnectStaticGroup
 {
-<#
+    <#
 	.SYNOPSIS
 		Get specified Static Group.
-	
+
 	.DESCRIPTION
 		Get specified Static Group.
-	
+
 	.PARAMETER StaticGroup
 		Name (wildcards supported) or GUID of the Static Group.
-	
+
 	.PARAMETER OrgUnitGuid
 		Valid GUID of a OrgUnit with Static Groups
-	
+
 	.OUTPUTS
 		bConnect.StaticGroup
 #>
-	[CmdletBinding(DefaultParameterSetName = 'GroupName')]
-	param
-	(
-		[Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'GroupName')]
-		[Alias('StaticGroupGuid')]
-		[string]
-		$StaticGroup,
-		
-		[Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'OrgName')]
-		[PsfValidatePattern('\b[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}\b', ErrorMessage = 'Failed to parse input as guid: {0}')]
-		[string]
-		$OrgUnitGuid
-	)
-	
-	begin
-	{
-		Assert-bConnectConnection
-	}
-	process
-	{
-		#region Search by Group
-		if ($StaticGroup)
-		{
-			If (Test-Guid $StaticGroup)
-			{
-				Invoke-bConnectGet -Controller "StaticGroups" -Data @{ Id = $StaticGroup } |
-				Select-PSFObject "ID as StaticGroupGuid", * |
-				Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
-			}
-			else
-			{
-				# fetching static groups with name is not supported; therefore we need a workaround for getting the specified static group...
-				if ((Get-bConnectVersion -bMSVersion).Major -ge 16)
-				{
-					# Search available since bMS 2016R1
-					Foreach ($group in (Search-bConnectStaticGroup -Term $StaticGroup))
-					{
-						Get-bConnectStaticGroup -StaticGroup $group.StaticGroupGuid
-					}
-				}
-				else
-				{
-					Invoke-bConnectGet -Controller "StaticGroups" |
-					Where-Object Name -Match $StaticGroup |
-					Select-PSFObject "ID as StaticGroupGuid", * |
-					Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
-				}
-			}
-		}
-		#endregion Search by Group
-		
-		#region Search by organization
-		elseif ($OrgUnitGuid)
-		{
-			Invoke-bConnectGet -Controller "StaticGroups" -Data @{ OrgUnitGuid = $OrgUnitGuid } |
-			Select-PSFObject "ID as StaticGroupGuid", * |
-			Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
-		}
-		#endregion Search by organization
-		
-		#region List all
-		else
-		{
-			Invoke-bConnectGet -Controller "StaticGroups" |
-			Select-PSFObject "ID as StaticGroupGuid", * |
-			Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
-		}
-		#endregion List all
-	}
+    [CmdletBinding(DefaultParameterSetName = 'GroupName')]
+    param
+    (
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'GroupName')]
+        [Alias('StaticGroupGuid')]
+        [string]
+        $StaticGroup,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true, ParameterSetName = 'OrgName')]
+        [PsfValidatePattern('\b[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}\b', ErrorMessage = 'Failed to parse input as guid: {0}')]
+        [string]
+        $OrgUnitGuid
+    )
+
+    begin
+    {
+        Assert-bConnectConnection
+    }
+    process
+    {
+        #region Search by Group
+        if ($StaticGroup)
+        {
+            If (Test-Guid $StaticGroup)
+            {
+                Invoke-bConnectGet -Controller "StaticGroups" -Data @{ Id = $StaticGroup } |
+                    Select-PSFObject "ID as StaticGroupGuid", * |
+                    Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
+            }
+            else
+            {
+                # fetching static groups with name is not supported; therefore we need a workaround for getting the specified static group...
+                if ((Get-bConnectVersion -bMSVersion).Major -ge 16)
+                {
+                    # Search available since bMS 2016R1
+                    Foreach ($group in (Search-bConnectStaticGroup -Term $StaticGroup))
+                    {
+                        Get-bConnectStaticGroup -StaticGroup $group.StaticGroupGuid
+                    }
+                }
+                else
+                {
+                    Invoke-bConnectGet -Controller "StaticGroups" |
+                        Where-Object Name -Match $StaticGroup |
+                        Select-PSFObject "ID as StaticGroupGuid", * |
+                        Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
+                }
+            }
+        }
+        #endregion Search by Group
+
+        #region Search by organization
+        elseif ($OrgUnitGuid)
+        {
+            Invoke-bConnectGet -Controller "StaticGroups" -Data @{ OrgUnit = $OrgUnitGuid } |
+                Select-PSFObject "ID as StaticGroupGuid", * |
+                Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
+        }
+        #endregion Search by organization
+
+        #region List all
+        else
+        {
+            Invoke-bConnectGet -Controller "StaticGroups" |
+                Select-PSFObject "ID as StaticGroupGuid", * |
+                Add-ObjectDetail -TypeName 'bConnect.StaticGroup' -WithID
+        }
+        #endregion List all
+    }
 }
