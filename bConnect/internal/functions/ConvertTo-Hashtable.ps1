63c1eb086e3ffe56bdc905e82459f81f2b3f455b
tobias.mueller@schriesheim.de <29426934+MuellerSchriesheim@users.noreply.github.com>
2019-03-27 11:22:57 +0100

    Fixed an Error in the conversion to Github. The cmdlet didn't convert the input object, instead it did only return an empty object.

--- bConnect/internal/functions/ConvertTo-Hashtable.ps1
+++ bConnect/internal/functions/ConvertTo-Hashtable.ps1
@@ -1,31 +1,31 @@
 ﻿function ConvertTo-Hashtable
 {
-<#
+    <#
 	.SYNOPSIS
 		Converts objects to hashtables
-	
+
 	.DESCRIPTION
 		Converts objects to hashtables
-	
+
 	.PARAMETER Object
 		The object(s) to convert to a hashtable
 #>
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-		[PSObject[]]
-		$Object
-	)
-	process
-	{
-		foreach ($objectItem in $Object)
-		{
-			$hashtable = [ordered]@{ }
-			foreach ($property in $objectItem)
-			{
-				$hashtable[$property.Name] = $property.Value
-			}
-			$hashtable
-		}
-	}
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [PSObject[]]
+        $Object
+    )
+    process
+    {
+        foreach ($objectItem in $Object)
+        {
+            $hashtable = [ordered]@{ }
+            foreach ($property in $objectItem.PSObject.Properties)
+            {
+                $hashtable[$property.Name] = $property.Value
+            }
+            $hashtable
+        }
+    }
 }
